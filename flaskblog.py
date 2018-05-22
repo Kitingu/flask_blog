d386587fc399d994e79491c5eda58eb5c4b8f95b
root <root@localhost.localdomain>
2018-05-22 10:52:17 +0300

    included layout template

---
+++ flaskblog.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+posts = [
+{
+"author":"benedict",
+"title": "first",
+"content":"first dummy content",
+"date_posted": "April20, 2018"
+},
+{
+"author":"Jayne",
+"title": "second",
+"content":"second dummy content",
+"date_posted": "April20, 2018"
+}
+]
+@app.route('/')
+def home():
+    return render_template('home.html',posts = posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html',title='kasee')
+
+if __name__ == '__main__':
+    app.run(debug=True)
